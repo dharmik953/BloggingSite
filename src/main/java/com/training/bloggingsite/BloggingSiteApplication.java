09c8388c2c690730b749aceed030f613eab44fcb
vishwas <vishwasshukla90@gmail.com>
2023-02-22 12:17:56 +0530

    Added all the required Dependencies

--- src/main/java/com/training/bloggingsite/BloggingSiteApplication.java
+++ src/main/java/com/training/bloggingsite/BloggingSiteApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BloggingSiteApplication {
 
     public static void main(String[] args) {
-
-
-        System.out.println("Hello");
         SpringApplication.run(BloggingSiteApplication.class, args);
     }
 
