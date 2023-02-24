b5b5fa026bf812ba3ea7a6e7cef9d826cd4f69f6
jeet <jeetpurohit989@gmail.com>
2023-02-24 09:05:43 +0530

    Added post entity crud operation(p artially....)

--- src/main/java/com/training/bloggingsite/contolleres/PostController.java
+++ src/main/java/com/training/bloggingsite/contolleres/PostController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-
+//comment
 @Controller
 public class PostController {
     @Autowired
