6eb6f76c242c09608ec4e95b01ace10665c6c1c9
dharmik <dharmikbhingradiya2001@gmail.com>
2023-02-24 09:17:38 +0530

    resolved conflicts

--- src/main/java/com/training/bloggingsite/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/training/bloggingsite/services/impl/CategoryServiceImpl.java
@@ -2,7 +2,13 @@ package com.training.bloggingsite.services.impl;
 
 import com.training.bloggingsite.entities.Category;
 import com.training.bloggingsite.repositories.CategoryRepositories;
+import com.training.bloggingsite.services.interfaces.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+
+import java.util.List;
+import java.util.Optional;
 
 import static java.util.Objects.isNull;
 
@@ -12,6 +18,10 @@ public class CategoryServiceImpl implements CategoryService {
     @Autowired
     CategoryRepositories repositories;
 
+
+    @Override
+    public Category addCategory(Category category) {
+        return null;
     }
 
     @Override
@@ -28,6 +38,11 @@ public class CategoryServiceImpl implements CategoryService {
     public Category getCategoryById(long id) {
         Optional<Category> category = repositories.findById(id);
         return category.get();
+    }
+
+    @Override
+    public Category addSubCategory(Category categoryName) {
+        return null;
     }
 
 
