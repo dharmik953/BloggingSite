171ca8afbe632f3e5f1db564a8a3079ac123ad4d
vishwas <vishwasshukla90@gmail.com>
2023-02-24 18:55:14 +0530

    Role Based Authentication Done

--- src/main/java/com/training/bloggingsite/BloggingSiteApplication.java
+++ src/main/java/com/training/bloggingsite/BloggingSiteApplication.java
@@ -1,9 +1,8 @@
 package com.training.bloggingsite;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class BloggingSiteApplication {
@@ -11,12 +10,5 @@ public class BloggingSiteApplication {
     public static void main(String[] args) {
         SpringApplication.run(BloggingSiteApplication.class, args);
     }
-
-    // Used for creating the Bean of ModelMapper class.
-    @Bean
-    public ModelMapper getModelMapperInstance(){
-        return new ModelMapper();
-    }
-
 
 }
--- src/main/java/com/training/bloggingsite/exceptions/UserEmailAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.training.bloggingsite.exceptions;
-
-public class UserEmailAlreadyExistsException extends RuntimeException{
-    public UserEmailAlreadyExistsException(String email){
-        super(
-                String.format("User with email %s is already present.",email)
-        );
-    }
-}
