4f29aafbbc11d4faae61469a5623431d90fd84b7
vishwas <vishwasshukla90@gmail.com>
2023-02-28 15:59:51 +0530

    Post partially done.

--- src/main/java/com/training/bloggingsite/repositories/PostRepository.java
+++ src/main/java/com/training/bloggingsite/repositories/PostRepository.java
@@ -8,10 +8,7 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-
     Post findPostByTitle(String title);
-
-  //  List<Post> findPostByVerifiedTrue();
-
-   // List<Post> findByVerifiedIsTrue();
+    List<Post> findPostsByIsVerifiedTrue();
+    List<Post> findPostsByIsVerifiedFalse();
 }
